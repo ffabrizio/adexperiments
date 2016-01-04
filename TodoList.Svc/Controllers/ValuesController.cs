b399d696ba28fe07b462f1898de830fe7af0bf3d
Fabio Fabrizio <f.fabrizio@syzygy.co.uk>
2016-01-04 17:55:51 +0000

    Initial commit

---
+++ TodoList.Svc/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace TodoList.Svc.Controllers
+{
+	[Authorize]
+    public class ValuesController : ApiController
+    {
+		public IEnumerable<string> Get()
+		{
+			// Ensure this is a valid user
+			if (ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/scope").Value != "user_impersonation")
+			{
+				throw new HttpResponseException(
+					new HttpResponseMessage {
+						StatusCode = HttpStatusCode.Unauthorized,
+						ReasonPhrase = "The Scope claim does not contain 'user_impersonation' or scope claim not found"
+					});
+			}
+
+			// Get the unique login for the user.
+			Claim subject = ClaimsPrincipal.Current.FindFirst(ClaimTypes.Name);
+
+			return new[] {
+				"Item 1",
+				"Item 2",
+				"Item 3",
+				subject.Value
+			};
+		}
+	}
+}
---
+++ TodoList.Svc/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+
+[assembly: OwinStartup(typeof(TodoList.Svc.Startup))]
+
+namespace TodoList.Svc
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+				new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+				{
+					Tenant = ConfigurationManager.AppSettings["ida:Tenant"],
+					TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidAudience = ConfigurationManager.AppSettings["ida:Audience"]
+					}
+				});
+		}
+	}
+}
